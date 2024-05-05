3ba406994a620cd2eee3d2f33b82ca7ca05c278d
TheJustRusik <7thejustrusik7@gmail.com>
2024-05-05 15:17:21 +0500

    New tables

--- src/main/resources/db/migration/V1.0__init.sql
+++ src/main/resources/db/migration/V1.0__init.sql
@@ -15,3 +15,54 @@ create table user_roles (
     role_id bigint references roles,
     primary key (user_id, role_id)
 );
+
+create table parking_type (
+    id bigserial primary key,
+    type varchar(255) not null unique
+);
+
+create table parking (
+    id bigserial primary key,
+    name varchar(255) not null unique,
+    lat double precision not null,
+    lon double precision not null,
+    price double precision check ( price >= 0 ) not null,
+    type_id bigint references parking_type,
+    max_capacity int,
+    current_workload int
+);
+
+create table parking_points (
+    parking_id bigint references parking,
+    lat double precision not null,
+    lon double precision not null,
+    primary key (lon, lat)
+);
+
+create table parking_likes (
+    parking_id bigint references parking,
+    user_id bigint references users,
+    is_dislike boolean not null default true,
+    primary key (parking_id, user_id)
+);
+
+create table parking_reviews (
+    parking_id bigint references parking,
+    user_id bigint references users,
+    stars smallint check ( stars >= 1 and stars <= 5),
+    review text,
+    primary key (parking_id, user_id)
+);
+
+insert into parking_type values
+                             (1, 'Underground'),
+                             (2, 'With roof'),
+                             (3, 'Outdoor'),
+                             (4, 'Multi storey'),
+                             (5, 'By the road');
+
+insert into roles values
+                      (1, 'User'),
+                      (2, 'Parking owner'),
+                      (3, 'Moderator'),
+                      (4, 'Admin')
