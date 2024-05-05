44f8ccfd3752b6d002bfea479ac015eeb7a03681
TheJustRusik <7thejustrusik7@gmail.com>
2024-05-05 16:12:15 +0500

    rename 2

--- main-service/src/test/java/we/hack/securityservice/MainServiceApplicationTests.java
+++ main-service/src/test/java/we/hack/securityservice/MainServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SecurityServiceApplicationTests {
+class MainServiceApplicationTests {
 
     @Test
     void contextLoads() {
