7894109a89c075eed3ce4d20518cc658d0f0c778
TheJustRusik <7thejustrusik7@gmail.com>
2024-05-05 10:43:12 +0500

    Initial commit

---
+++ src/main/java/org/kenuki/parkingpro/ParkingProApplication.java
@@ -0,0 +1,13 @@
+package org.kenuki.parkingpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingProApplication.class, args);
+    }
+
+}
